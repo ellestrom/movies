dd32c0181a046eaee796ab3245e729de9e7ee25c
ellestrom <alexander@ellestrom.se>
2023-05-11 23:25:39 +0200

    Update Movie.js
    Fixed the stars

--- src/Movie.js
+++ src/Movie.js
@@ -7,21 +7,23 @@ export default function Movie(props) {
   const stars = [];
 
   for (let i = 0; i < rating; i++) {
-    stars.push(<img key={i} src={star} alt="star" />);
+    stars.push(<img key={i} className="img-fluid" src={star} alt="star" />);
   }
 
   return (
-    <li className="list-group-item">
+    <li className="list-group-item d-flex justify-content-between align-items-center">
       {props.item.title}
-      {stars}
-      <img
-        className="float-end"
-        src={del}
-        alt="delete"
-        onClick={() => {
-          props.deleteMovie(props.item.id);
-        }}
-      />
+      <div>
+        {stars}
+        <img
+          className="img-fluid"
+          src={del}
+          alt="delete"
+          onClick={() => {
+            props.deleteMovie(props.item.id);
+          }}
+        />
+      </div>
     </li>
   );
 }
