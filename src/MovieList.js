1fede49efa152b0c7d092c208ec290c7245e8473
ellestrom <alexander@ellestrom.se>
2023-05-11 23:45:49 +0200

    Update MovieList.js
    Updated sorting

--- src/MovieList.js
+++ src/MovieList.js
@@ -5,6 +5,8 @@ export default function MovieList() {
   const [movies, setMovies] = useState(() => JSON.parse(localStorage.getItem("movies")) || []);
   const inputRef = useRef();
   const ratingRef = useRef();
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortType, setSortType] = useState("title");
 
   function addMovie(event) {
     event.preventDefault();
@@ -30,12 +32,13 @@ export default function MovieList() {
   function sortMovies(order) {
     const sortedMovies = [...movies].sort((a, b) => {
       if (order === "asc") {
-        return a.title.localeCompare(b.title, "sv");
+        return a[sortType].localeCompare(b[sortType], "sv");
       } else {
-        return b.title.localeCompare(a.title, "sv");
+        return b[sortType].localeCompare(a[sortType], "sv");
       }
     });
     setMovies(sortedMovies);
+    setSortOrder(order === "asc" ? "desc" : "asc");
   }
 
   useEffect(() => {
@@ -70,9 +73,9 @@ export default function MovieList() {
           <button type="submit" className="btn btn-success mt-3">Spara film</button>
         </fieldset>
       </form>
-      <div className="sort-buttons mt-3">
-        <button className="btn btn-primary" onClick={() => sortMovies("asc")}>Sortera A-Ö</button>
-        <button className="btn btn-primary" onClick={() => sortMovies("desc")}>Sortera Ö-A</button>
+      <div className="sort-buttons mt-4 mb-4">
+        <button className="btn btn-primary" onClick={() => sortMovies(sortOrder)}>Sortera {sortOrder === "asc" ? "A-Ö" : "Ö-A"}</button>
+        <button className="btn btn-primary" onClick={() => {setSortType("rating"); sortMovies(sortOrder); setSortType("title");}}>Sortera Betyg {sortOrder === "asc" ? "1-5" : "5-1"}</button>
       </div>
       <ul className="list-group">
         {movies.map((movie) => (
